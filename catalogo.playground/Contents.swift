62d2f3a28ce6607a6f82d2325efb2c76fd581247
Larissa Diniz <larissa.dtm@gmail.com>
2019-12-16 17:08:27 -0300

    acervo series

--- catalogo.playground/Contents.swift
+++ catalogo.playground/Contents.swift
@@ -16,14 +16,13 @@ class Serie{
     }
 }
 
-
-
-var GOT = Serie(nome: "Game of Thrones", ano: 2011, temporadas: 8, genero: "fantasia")
+var GOT = Serie(nome: "Game of Thrones", ano: 2011, temporadas: 8, genero: "Fantasia")
 var BB = Serie(nome: "Breaking Bad", ano: 2008, temporadas: 5, genero: "Drama")
 var Vikings = Serie(nome: "Vikings", ano: 2013, temporadas: 5, genero: "Drama")
-var AHS = Serie(nome: "American Horror Story", ano: 2011, temporadas: Int, genero: <#T##String#>)
-AHS.nome = "American Horror Story"
-AHS.ano = 2011
-AHS.temporadas = 9
-AHS.genero = "Horror"
-
+var AHS = Serie(nome: "American Horror Story", ano: 2011, temporadas: 9, genero: "Horror")
+var anne = Serie(nome: "Anne with an E", ano: 2017, temporadas: 2, genero: "Drama")
+var Supernatural = Serie(nome: "Supernatural", ano: 2005, temporadas: 15, genero: "Terror")
+var house = Serie(nome: "House", ano: 2004, temporadas: 8, genero: "Comédia")
+var THM = Serie(nome: "Two and a Half Men", ano: 2003, temporadas: 12, genero: "Comédia")
+var Chris = Serie(nome: "Todo Mundo Odeia o Chris", ano: 2005, temporadas: 4, genero: "Comédia")
+var Hannibal = Serie(nome: "Hannibal", ano: 3, temporadas: 2013, genero: "Terror")
