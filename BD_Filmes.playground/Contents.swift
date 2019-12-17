76bda6d3cfe1706653ea350b5b21c495d981fead
Vitor Costa <deividevitor565@gmail.com>
2019-12-17 16:08:59 -0300

    Added to function to add Favorites

--- BD_Filmes.playground/Contents.swift
+++ BD_Filmes.playground/Contents.swift
@@ -1,11 +1,35 @@
 import UIKit
 
 class Catalog{
-    var films: [Film]
+    var films: [Film] = []
+    var favorites: [Film] = []
     
-    init(films: [Film]) {
-        self.films = films
+    func add_Film(name: String, year: Int, rating: String, genre: String){
+        let film = Film(name: name, year: year, rating: rating, genre: genre)
+        
+        films.append(film)
     }
+    
+    func addToFavorites(name: String){
+        for film in films{
+            if name == film.name{
+                favorites.append(film)
+            }
+        }
+    }
+    
+    func showFilms(){
+        for film in films{
+            print("Filme: \(film.name) Ano: \(film.year) Classificação: \(film.rating) Gênero: \(film.genre)")
+        }
+    }
+    
+    func showFavorites(){
+        for fav in favorites{
+            print("Filme: \(fav.name) Ano: \(fav.year) Classificação: \(fav.rating) Gênero: \(fav.genre)")
+        }
+    }
+    
 }
 
 class Film{
@@ -22,35 +46,20 @@ class Film{
     }
 }
 
-var allCatalog: [Any] = []
+let catalogo = Catalog()
 
-var listFilms: [Film] = []
+catalogo.add_Film(name: "Resident Evil 2: Apocalipse", year: 2008, rating: "16", genre: "Ficção")
+catalogo.add_Film(name: "Senhor dos Anéis - A Sociedade do Anel", year: 2001, rating: "12", genre: "Fantasia")
+catalogo.add_Film(name: "Harry Potter - As Relíquias da Morte", year: 2010, rating: "12", genre: "Ficção")
+catalogo.add_Film(name: "Star Wars - O Despertar da Força", year: 2015, rating: "12", genre: "Ficção")
+catalogo.add_Film(name: "A fantástica Fábrica de Chocolate", year: 2005, rating: "L", genre: "Ficção")
+catalogo.add_Film(name: "Oh Ramona", year: 2019, rating: "16", genre: "Romance")
+catalogo.add_Film(name: "A Procura da Felicidade", year: 2006, rating: "L", genre: "Drama")
+catalogo.add_Film(name: "Família do Bagulho", year: 2014, rating: "14", genre: "Comédia")
+catalogo.add_Film(name: "Shrek Terceiro", year: 2007, rating: "L", genre: "Comédia")
+catalogo.add_Film(name: "Esquadrão Suicida", year: 2016, rating: "12", genre: "Ação")
+catalogo.add_Film(name: "Onde Está Segunda", year: 2017, rating: "16", genre: "Ação")
 
-allCatalog.append(listFilms)
-
-func add_Film(name: String, year: Int, rating: String, genre: String){
-    let film = Film(name: name, year: year, rating: rating, genre: genre)
-    
-    listFilms.append(film)
-    
-}
-
-add_Film(name: "Resident Evil 2: Apocalipse", year: 2008, rating: "16", genre: "Ficção")
-add_Film(name: "Senhor dos Anéis - A Sociedade do Anel", year: 2001, rating: "12", genre: "Fantasia")
-add_Film(name: "Harry Potter - As Relíquias da Morte", year: 2010, rating: "12", genre: "Ficção")
-add_Film(name: "Star Wars - O Despertar da Força", year: 2015, rating: "12", genre: "Ficção")
-add_Film(name: "A fantástica Fábrica de Chocolate", year: 2005, rating: "L", genre: "Ficção")
-add_Film(name: "Oh Ramona", year: 2019, rating: "16", genre: "Romance")
-add_Film(name: "A Procura da Felicidade", year: 2006, rating: "L", genre: "Drama")
-add_Film(name: "Família do Bagulho", year: 2014, rating: "14", genre: "Comédia")
-add_Film(name: "Shrek Terceiro", year: 2007, rating: "L", genre: "Comédia")
-add_Film(name: "Esquadrão Suicida", year: 2016, rating: "12", genre: "Ação")
-add_Film(name: "Onde Está Segunda", year: 2017, rating: "16", genre: "Ação")
-
-for filme in listFilms{
-    print("Filme: \(filme.name) Ano: \(filme.year) Classificação: \(filme.rating) Gênero: \(filme.genre)")
-}
-
-for item in allCatalog{
-    
-}
+catalogo.showFilms()
+catalogo.addToFavorites(name: "Oh Ramona")
+catalogo.showFavorites()
