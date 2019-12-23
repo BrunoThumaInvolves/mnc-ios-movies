1675d42734a8816dceb33c86ac7f15b5938e8a88
Vitor Costa <deividevitor565@gmail.com>
2019-12-23 13:13:32 -0300

    Added class and theree var

--- Catalogo/Catalogo/ViewController.swift
+++ Catalogo/Catalogo/ViewController.swift
@@ -27,15 +27,33 @@ class ViewController: UIViewController {
         catalogo.addSerie(nome: "Todo Mundo Odeia o Chris", ano: 2005, temporadas: 4, genero: "Comédia")
         catalogo.addSerie(nome: "Hannibal", ano: 3, temporadas: 2013, genero: "Terror")
         catalogo.addSerie(nome: "Grey's Anatomy", ano: 2005, temporadas: 15, genero: "Romance")
-
+        catalogo.addFilm(name: "Resident Evil 2: Apocalipse", year: 2008, rating: "16", genre: "Ficção")
+        catalogo.addFilm(name: "Senhor dos Anéis - A Sociedade do Anel", year: 2001, rating: "12", genre: "Fantasia")
+        catalogo.addFilm(name: "Harry Potter - As Relíquias da Morte", year: 2010, rating: "12", genre: "Ficção")
+        catalogo.addFilm(name: "Star Wars - O Despertar da Força", year: 2015, rating: "12", genre: "Ficção")
+        catalogo.addFilm(name: "A fantástica Fábrica de Chocolate", year: 2005, rating: "L", genre: "Ficção")
+        catalogo.addFilm(name: "Oh Ramona", year: 2019, rating: "16", genre: "Romance")
+        catalogo.addFilm(name: "A Procura da Felicidade", year: 2006, rating: "L", genre: "Drama")
+        catalogo.addFilm(name: "Família do Bagulho", year: 2014, rating: "14", genre: "Comédia")
+        catalogo.addFilm(name: "Shrek Terceiro", year: 2007, rating: "L", genre: "Comédia")
+        catalogo.addFilm(name: "Esquadrão Suicida", year: 2016, rating: "12", genre: "Ação")
+        catalogo.addFilm(name: "Onde Está Segunda", year: 2017, rating: "16", genre: "Ação")
+        catalogo.addFavorites(name: "Oh Ramona")
+        catalogo.addFavorites(name: "Esquadrão Suicida")
+        catalogo.addFavorites(name: "Onde Está Segunda")
+        catalogo.addWish(tittle: "Malévola", year: 2014, director: "Robert Stromberg", description: "Esse filme é muito bom!")
+        
+        catalogo.removeMovie(name: "Família do Bagulho")
         catalogo.removeSerie(nome: "Hannibal")
+        catalogo.removeFavorite(name: "Oh Ramona")
+        catalogo.removeWish(tittle: "Malévola")
+        
+        catalogo.showFilms()
+        catalogo.showFavorites()
+        catalogo.showWishes()
+        
 
         catalogo.mostrarSeries()
 
     }
-
-
 }
-
-
-
--- Catalogo/Catalogo/classCatalogo.swift
+++ Catalogo/Catalogo/classCatalogo.swift
@@ -11,10 +11,67 @@ import Foundation
 class Catalogo {
     
     var listaSeries:[Serie] = []
+    var movies: [Movie] = []
+    var wishes:[Wishes] = []
+    var favorites: [Movie] = []
+    
+    func addFilm(name: String, year: Int, rating: String, genre: String){
+        let movie = Movie(name: name, year: year, rating: rating, genre: genre)
+        movies.append(movie)
+    }
 
     func addSerie(nome:String, ano:Int, temporadas:Int, genero:String){
         var serie = Serie(nome: nome, ano: ano, temporadas: temporadas, genero: genero)
         listaSeries.append(serie)
+    }
+    
+    func addFavorites(name: String){
+        for fav in movies{
+            if name == fav.name{
+                favorites.append(fav)
+            }
+        }
+    }
+    
+    func addWish(tittle: String, year: Int, director: String, description: String){
+        let wish = Wishes(tittle: tittle, year: year, director: director, description: description)
+        wishes.append(wish)
+        print("Pedido adicionado!")
+    }
+    
+    func showFilms(){
+        print("\nEsses são os seus filmes:\n")
+        for movie in movies{
+            print("Filme: \(movie.name) Ano: \(movie.year) Classificação: \(movie.rating) Gênero: \(movie.genre)")
+        }
+    }
+    
+    func mostrarSeries(){
+        for serie in listaSeries{
+            print("Nome: \(serie.nome); Ano: \(serie.ano); Temporadas: \(serie.temporadas); Gênero: \(serie.genero)")
+        }
+    }
+    
+    func showFavorites(){
+        print("\nEsses são os seus favoritos:\n")
+        for fav in favorites{
+            print("Filme: \(fav.name) Ano: \(fav.year) Classificação: \(fav.rating) Gênero: \(fav.genre)")
+        }
+    }
+    
+    func showWishes(){
+        print("\nEsses são os seus pedidos:\n")
+        for wish in wishes{
+            print("Filme: \(wish.tittle) Ano: \(wish.year) Diretor: \(wish.director) Descrição do pedido: \(wish.description)")
+        }
+    }
+    
+    func removeMovie(name: String){
+        for (index ,movie) in movies.enumerated(){
+            if name == movie.name{
+                movies.remove(at: index)
+            }
+        }
     }
     
     func removeSerie(nome:String){
@@ -25,10 +82,20 @@ class Catalogo {
         }
     }
     
-    func mostrarSeries(){
-        for serie in listaSeries{
-            print("Nome: \(serie.nome); Ano: \(serie.ano); Temporadas: \(serie.temporadas); Gênero: \(serie.genero)")
-          }
+    func removeFavorite(name: String){
+        for (index ,fav) in favorites.enumerated(){
+            if name == fav.name{
+                favorites.remove(at: index)
+            }
+        }
+    }
+    
+    func removeWish(tittle: String){
+        for (index ,wish) in wishes.enumerated(){
+            if tittle == wish.tittle{
+                wishes.remove(at: index)
+            }
+        }
     }
 }
 
